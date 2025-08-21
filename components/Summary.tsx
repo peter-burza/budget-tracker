84b47e4a5d3c1e45c5f04833ccb5937a033aae5e
Peter Burza <peterburza48@gmail.com>
2025-08-21 09:01:53 +0000

    styling summary

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -34,30 +34,28 @@ const Summary: React.FC<SummaryProps> = ({ dateFilteredTransactions, currency, t
         <div id="summary" className="flex flex-col items-center gap-2 w-full">
             <h3>Summary</h3>
             <p>Basic info of the selected period.</p>
-            <div className="flex justify-between gap-2">
-                <div id="basic-summary-info" className="flex flex-col gap-[8px]">
-                    <div className="flex gap-2 items-center">
+            <div id="basic-summary-info" className="flex flex-col w-full justify-between gap-0.25">
+                    <div className="flex gap-2 w-full items-center justify-center bg-[var(--color-list-bg-green)] text-green-100 p-1 border-1 border-[var(--color-dark-blue)]">
                         <h4>Income:</h4>
                         <div className="flex">
                             <h4>{fancyNumber(totalIncome)}</h4>
                             <h4 className="-mt-[0.15rem]">{currency}</h4>
                         </div>
                     </div>
-                    <div className="flex gap-2 items-center">
+                    <div className="flex gap-2 w-full items-center justify-center bg-[var(--color-list-bg-red)] text-red-300 p-1 border-1 border-[var(--color-dark-blue)]">
                         <h4>Expence:</h4>
                         <div className="flex">
                             <h4>- {fancyNumber(totalExpense)}</h4>
                             <h4 className="-mt-[0.15rem]">{currency}</h4>
                         </div>
                     </div>
-                    <div className="flex gap-2 items-center">
+                    <div className="flex gap-2 w-full items-center justify-center bg-sky-800 text-sky-300 p-1 border-1 border-[var(--color-dark-blue)]">
                         <h5>Net Balance:</h5>
                         <div className="flex">
                             <h5>{totalExpense > totalIncome && '- '} {fancyNumber(calculateNetBalance(totalIncome, totalExpense))}</h5>
                             <h5 className="-mt-[0.15rem]">{currency}</h5>
                         </div>
                     </div>
-                </div>
             </div>
         </div>
     )
