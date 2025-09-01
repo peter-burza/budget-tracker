26327cc625e1576ca61c64314bd374f1b49b107f
Martin Burza <martin.burza@radixal.net>
2025-09-01 22:06:39 +0200

    Make Transaction Id nullable / not required

--- components/Entry.tsx
+++ components/Entry.tsx
@@ -1,112 +1,152 @@
 'use client'
 
-import { Category, Transaction } from "@/app/interfaces/Transaction"
-import React, { useState } from "react"
-import ResponsiveDatePicker from "./ui/ResponsiveDatePicker"
-import dayjs from "dayjs"
+import { Category, Transaction } from '@/app/interfaces/Transaction'
+import React, { useState } from 'react'
+import ResponsiveDatePicker from './ui/ResponsiveDatePicker'
+import dayjs from 'dayjs'
 
 interface EntryProps {
-    saveTransaction: (transaction: Transaction) => void
-    isLoading: boolean
+  saveTransaction: (transaction: Transaction) => void
+  isLoading: boolean
 }
 
 const Entry: React.FC<EntryProps> = ({ saveTransaction, isLoading }) => {
-    const defaultTrVals = {}
-    const [newTransaction, setNewTransaction] = useState<Transaction>({
-        id: crypto.randomUUID(),
-        amount: 0,
-        type: "+",
-        date: dayjs(Date.now()).format("YYYY-MM-DD"),
-        category: Category.Salary,
-    })
-    const cantAddEntry: boolean | undefined = newTransaction.amount === 0 ? true : false
+  const defaultTrVals = {}
 
-    function displayAmount(amount: number): string {
-        return amount === 0 ? '' : amount.toString()
-    }
+  // TODO: Break newTransaction into individual states
+  // const [amout, setAmount] = useState<number>(0)
+  const [newTransaction, setNewTransaction] = useState<Transaction>({
+    // id: crypto.randomUUID(),
+    amount: 0,
+    type: '-',
+    date: dayjs(Date.now()).format('YYYY-MM-DD'),
+    category: Category.Other
+  })
+  const cantAddEntry: boolean | undefined =
+    newTransaction.amount === 0 ? true : false
 
-    function setAmount(value: string): void {
-        const parsedValue = parseFloat(value)
-        const validValue = Number.isNaN(parsedValue) ? 0 : parsedValue
-        setNewTransaction(prev => ({
-            ...prev, amount: validValue
-        }))
-    }
+  function displayAmount(amount: number): string {
+    return amount === 0 ? '' : amount.toString()
+  }
 
-    function setType(value: string): void {
-        let result: "+" | "-"
-        if (value === "+")
-            result = value
-        else
-            result = "-"
-        setNewTransaction(prev => ({
-            ...prev, type: result
-        }))
-    }
+  function setAmount(value: string): void {
+    const parsedValue = parseFloat(value)
+    const validValue = Number.isNaN(parsedValue) ? 0 : parsedValue
+    setNewTransaction((prev) => ({
+      ...prev,
+      amount: validValue
+    }))
+  }
 
-    function setCategory(value: string): void {
-        let result: Category | undefined = Object.values(Category).find(c => c === value)
-        if (result !== undefined) {
-            setNewTransaction(prev => ({
-                ...prev, category: result
-            }))
-        }
-    }
+  function setType(value: string): void {
+    let result: '+' | '-'
+    if (value === '+') result = value
+    else result = '-'
+    setNewTransaction((prev) => ({
+      ...prev,
+      type: result
+    }))
+  }
 
-    function setDate(value: dayjs.Dayjs): void {
-        const dateOnly: string = value.format("YYYY-MM-DD")
-        setNewTransaction(prev => ({
-            ...prev, date: dateOnly
-        }))
-    }
-
-    function setDescription(value: string): void {
-        setNewTransaction(prev => ({ ...prev, description: value }))
-    }
-
-
-    return (
-        <div id="transaction-entry" className="flex flex-col items-center gap-2 bg-[var(--background-muted)] rounded-md p-3">
-            <h3>New Entry</h3>
-            <div className="flex flex-col gap-1 max-w-[232px] w-full">
-                <p>Amount:</p>
-                <input value={displayAmount(newTransaction.amount)} onChange={(e) => { setAmount(e.target.value) }} type="number" step="any" placeholder="e.g. 4.99" />
-            </div>
-            <div className="flex flex-col gap-1 max-w-[232px] w-full">
-                <p>Type:</p>
-                <select value={newTransaction.type} onChange={(e) => { setType(e.target.value) }}>
-                    <option value="+">Income</option>
-                    <option value="-">Expense</option>
-                </select>
-            </div>
-            <div className="flex flex-col gap-1 max-w-[232px] w-full">
-                <p>Category:</p>
-                <select value={newTransaction.category} onChange={e => { setCategory(e.target.value) }}>
-                    {Object.values(Category).map(c => (
-                        <option key={c} value={c}>{c}</option>
-                    ))}
-                </select>
-            </div>
-            <div className="flex flex-col gap-1 max-w-[232px] w-full">
-                <p className="-mb-2">Date:</p>
-                <ResponsiveDatePicker setTransactionDate={setDate} />
-            </div>
-            <div className="flex flex-col gap-1 max-w-[232px] w-full">
-                <p className="">Description:</p>
-                <textarea onChange={e => { setDescription(e.target.value) }} className="bg-[var(--foreground)] text-[var(--background)] outline-0 p-2 px-3 rounded-sm" placeholder="Transaction detail"></textarea>
-            </div>
-            <button
-                className="secondary-btn disabled:opacity-50"
-                disabled={cantAddEntry || isLoading}
-                title={cantAddEntry ? "Please enter amount" : ""}
-                onClick={() => {
-                    saveTransaction({ ...newTransaction })
-                    setNewTransaction({ ...newTransaction, id: crypto.randomUUID() }) // Change the id, for next entry
-                }} >
-                <h5>{isLoading === true ? 'Saving...' : 'Add Transaction'}</h5>
-            </button>
-        </div>
+  function setCategory(value: string): void {
+    let result: Category | undefined = Object.values(Category).find(
+      (c) => c === value
     )
+    if (result !== undefined) {
+      setNewTransaction((prev) => ({
+        ...prev,
+        category: result
+      }))
+    }
+  }
+
+  function setDate(value: dayjs.Dayjs): void {
+    const dateOnly: string = value.format('YYYY-MM-DD')
+    setNewTransaction((prev) => ({
+      ...prev,
+      date: dateOnly
+    }))
+  }
+
+  function setDescription(value: string): void {
+    setNewTransaction((prev) => ({ ...prev, description: value }))
+  }
+
+  return (
+    <div
+      id="transaction-entry"
+      className="flex flex-col items-center gap-2 bg-[var(--background-muted)] rounded-md p-3"
+    >
+      <h3>New Entry</h3>
+      <div className="flex flex-col gap-1 max-w-[232px] w-full">
+        <p>Amount:</p>
+        <input
+          value={displayAmount(newTransaction.amount)}
+          onChange={(e) => {
+            setAmount(e.target.value)
+          }}
+          type="number"
+          step="any"
+          placeholder="e.g. 4.99"
+        />
+      </div>
+      <div className="flex flex-col gap-1 max-w-[232px] w-full">
+        <p>Type:</p>
+        <select
+          value={newTransaction.type}
+          onChange={(e) => {
+            setType(e.target.value)
+          }}
+        >
+          <option value="+">Income</option>
+          <option value="-">Expense</option>
+        </select>
+      </div>
+      <div className="flex flex-col gap-1 max-w-[232px] w-full">
+        <p>Category:</p>
+        <select
+          value={newTransaction.category}
+          onChange={(e) => {
+            setCategory(e.target.value)
+          }}
+        >
+          {Object.values(Category).map((c) => (
+            <option
+              key={c}
+              value={c}
+            >
+              {c}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-col gap-1 max-w-[232px] w-full">
+        <p className="-mb-2">Date:</p>
+        <ResponsiveDatePicker setTransactionDate={setDate} />
+      </div>
+      <div className="flex flex-col gap-1 max-w-[232px] w-full">
+        <p className="">Description:</p>
+        <textarea
+          onChange={(e) => {
+            setDescription(e.target.value)
+          }}
+          className="bg-[var(--foreground)] text-[var(--background)] outline-0 p-2 px-3 rounded-sm"
+          placeholder="Transaction detail"
+        ></textarea>
+      </div>
+      <button
+        className="secondary-btn disabled:opacity-50"
+        disabled={cantAddEntry || isLoading}
+        title={cantAddEntry ? 'Please enter amount' : ''}
+        onClick={() => {
+          saveTransaction({ ...newTransaction })
+          setNewTransaction({ ...newTransaction, id: crypto.randomUUID() }) // Change the id, for next entry
+        }}
+      >
+        <h5>{isLoading === true ? 'Saving...' : 'Add Transaction'}</h5>
+      </button>
+    </div>
+  )
 }
 
 export default Entry
--- src/app/interfaces/Transaction.tsx
+++ src/app/interfaces/Transaction.tsx
@@ -1,30 +1,30 @@
-import { JSX } from "@emotion/react/jsx-runtime";
+import { JSX } from '@emotion/react/jsx-runtime'
 
 export enum Category {
-  Salary = "Salary",
-  Rent = "Rent",
-  Groceries = "Groceries",
-  Food = "Food",
-  InternetPhone = "Internet & Phone",
-  HealthInsurance = "Health Insurance",
-  Savings = "Savings",
-  FixedExp = "Fixed expenses",
-  Shopping = "Shopping",
-  Entertainment = "Entertainment",
-  CarMaintenance = "Car Maintenance",
-  KidsSchool = "Kids & School",
-  Pets = "Pets",
-  GymFitness = "Gym & Fitness",
-  StreamingServices = "Streaming Services",
-  Home = "Home",
-  Investment = "Investment",
-  Vacation = "Vacation",
-  Birthdays = "Birthdays",
-  Christmass = "Christmass",
-  Party = "Party",
-  Date = "Date",
-  Garden = "Garden",
-  Other = "Other",
+  Salary = 'Salary',
+  Rent = 'Rent',
+  Groceries = 'Groceries',
+  Food = 'Food',
+  InternetPhone = 'Internet & Phone',
+  HealthInsurance = 'Health Insurance',
+  Savings = 'Savings',
+  FixedExp = 'Fixed expenses',
+  Shopping = 'Shopping',
+  Entertainment = 'Entertainment',
+  CarMaintenance = 'Car Maintenance',
+  KidsSchool = 'Kids & School',
+  Pets = 'Pets',
+  GymFitness = 'Gym & Fitness',
+  StreamingServices = 'Streaming Services',
+  Home = 'Home',
+  Investment = 'Investment',
+  Vacation = 'Vacation',
+  Birthdays = 'Birthdays',
+  Christmass = 'Christmass',
+  Party = 'Party',
+  Date = 'Date',
+  Garden = 'Garden',
+  Other = 'Other'
 }
 
 // export const CategoryIcons = {
@@ -55,36 +55,156 @@ export enum Category {
 // }
 
 export const CategoryIcons: Record<Category, JSX.Element> = {
-  [Category.Salary]: <i className="fa-solid fa-money-check-dollar" title="Salary"></i>,
-  [Category.Rent]: <i className="fa-solid fa-person-cane" title="Rent"></i>,
-  [Category.Groceries]: <i className="fa-solid fa-cart-shopping" title="Groceries"></i>,
-  [Category.Food]: <i className="fa-solid fa-utensils" title="Food"></i>,
-  [Category.InternetPhone]: <i className="fa-solid fa-globe" title="Internet & Phone"></i>,
-  [Category.HealthInsurance]: <i className="fa-solid fa-notes-medical" title="Health Insurance"></i>,
-  [Category.Savings]: <i className="fa-solid fa-piggy-bank" title="Savings"></i>,
-  [Category.FixedExp]: <i className="fa-solid fa-wallet" title="Fixed Expenses"></i>,
-  [Category.Shopping]: <i className="fa-solid fa-bag-shopping" title="Shopping"></i>,
-  [Category.Entertainment]: <i className="fa-solid fa-microphone-lines" title="Entertainment"></i>,
-  [Category.CarMaintenance]: <i className="fa-solid fa-car" title="Car Maintenance"></i>,
-  [Category.KidsSchool]: <i className="fa-solid fa-child" title="Kids & School"></i>,
-  [Category.Pets]: <i className="fa-solid fa-paw" title="Pets"></i>,
-  [Category.GymFitness]: <i className="fa-solid fa-dumbbell" title="Gym & Fitness"></i>,
-  [Category.StreamingServices]: <i className="fa-solid fa-tv" title="Streaming Services"></i>,
-  [Category.Home]: <i className="fa-solid fa-house-chimney" title="Home"></i>,
-  [Category.Investment]: <i className="fa-solid fa-money-bill-trend-up" title="Investment"></i>,
-  [Category.Vacation]: <i className="fa-solid fa-umbrella-beach" title="Vacation"></i>,
-  [Category.Birthdays]: <i className="fa-solid fa-cake-candles" title="Birthdays"></i>,
-  [Category.Christmass]: <i className="fa-solid fa-gift" title="Christmas"></i>,
-  [Category.Party]: <i className="fa-solid fa-champagne-glasses" title="Party"></i>,
-  [Category.Date]: <i className="fa-solid fa-heart" title="Date"></i>,
-  [Category.Garden]: <i className="fa-solid fa-tree" title="Garden"></i>,
-  [Category.Other]: <i className="fa-solid fa-star-of-life" title="Other"></i>,
-};
+  [Category.Salary]: (
+    <i
+      className="fa-solid fa-money-check-dollar"
+      title="Salary"
+    ></i>
+  ),
+  [Category.Rent]: (
+    <i
+      className="fa-solid fa-person-cane"
+      title="Rent"
+    ></i>
+  ),
+  [Category.Groceries]: (
+    <i
+      className="fa-solid fa-cart-shopping"
+      title="Groceries"
+    ></i>
+  ),
+  [Category.Food]: (
+    <i
+      className="fa-solid fa-utensils"
+      title="Food"
+    ></i>
+  ),
+  [Category.InternetPhone]: (
+    <i
+      className="fa-solid fa-globe"
+      title="Internet & Phone"
+    ></i>
+  ),
+  [Category.HealthInsurance]: (
+    <i
+      className="fa-solid fa-notes-medical"
+      title="Health Insurance"
+    ></i>
+  ),
+  [Category.Savings]: (
+    <i
+      className="fa-solid fa-piggy-bank"
+      title="Savings"
+    ></i>
+  ),
+  [Category.FixedExp]: (
+    <i
+      className="fa-solid fa-wallet"
+      title="Fixed Expenses"
+    ></i>
+  ),
+  [Category.Shopping]: (
+    <i
+      className="fa-solid fa-bag-shopping"
+      title="Shopping"
+    ></i>
+  ),
+  [Category.Entertainment]: (
+    <i
+      className="fa-solid fa-microphone-lines"
+      title="Entertainment"
+    ></i>
+  ),
+  [Category.CarMaintenance]: (
+    <i
+      className="fa-solid fa-car"
+      title="Car Maintenance"
+    ></i>
+  ),
+  [Category.KidsSchool]: (
+    <i
+      className="fa-solid fa-child"
+      title="Kids & School"
+    ></i>
+  ),
+  [Category.Pets]: (
+    <i
+      className="fa-solid fa-paw"
+      title="Pets"
+    ></i>
+  ),
+  [Category.GymFitness]: (
+    <i
+      className="fa-solid fa-dumbbell"
+      title="Gym & Fitness"
+    ></i>
+  ),
+  [Category.StreamingServices]: (
+    <i
+      className="fa-solid fa-tv"
+      title="Streaming Services"
+    ></i>
+  ),
+  [Category.Home]: (
+    <i
+      className="fa-solid fa-house-chimney"
+      title="Home"
+    ></i>
+  ),
+  [Category.Investment]: (
+    <i
+      className="fa-solid fa-money-bill-trend-up"
+      title="Investment"
+    ></i>
+  ),
+  [Category.Vacation]: (
+    <i
+      className="fa-solid fa-umbrella-beach"
+      title="Vacation"
+    ></i>
+  ),
+  [Category.Birthdays]: (
+    <i
+      className="fa-solid fa-cake-candles"
+      title="Birthdays"
+    ></i>
+  ),
+  [Category.Christmass]: (
+    <i
+      className="fa-solid fa-gift"
+      title="Christmas"
+    ></i>
+  ),
+  [Category.Party]: (
+    <i
+      className="fa-solid fa-champagne-glasses"
+      title="Party"
+    ></i>
+  ),
+  [Category.Date]: (
+    <i
+      className="fa-solid fa-heart"
+      title="Date"
+    ></i>
+  ),
+  [Category.Garden]: (
+    <i
+      className="fa-solid fa-tree"
+      title="Garden"
+    ></i>
+  ),
+  [Category.Other]: (
+    <i
+      className="fa-solid fa-star-of-life"
+      title="Other"
+    ></i>
+  )
+}
 
 export interface Transaction {
-  readonly id: string
+  readonly id?: string
   amount: number
-  type: "+" | "-"
+  type: '+' | '-'
   date: string
   category: Category
   description?: string
