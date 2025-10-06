80c6b2ac3bfd7fe3c8368fac3b630409189ef6f7
Qalint <peterburza48@gmail.com>
2025-10-06 21:57:46 +0200

    Footer finished for now, also changed favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
     return (
         <head>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-
+            <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         </head>
     )
 }
